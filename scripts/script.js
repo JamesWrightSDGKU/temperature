91c2869146e5a73fcce20fd5ad0655ea0cf3413d
JamesWrightSDGKU <James.Wright@sdgku.edu>
2025-01-22 19:51:44 -0800

    add .

---
+++ scripts/script.js
@@ -0,0 +1,73 @@
+console.log("Variables");
+// keyword nameVar = value;
+// var, let, const
+
+// string
+var firstName = "James";
+var lastName = "Wright";
+
+// numerical values
+let myAge = 34;
+let myHeight = 68;
+
+// boolean
+let isStudent = true;
+
+// display the values on the console
+console.log("My name is " + firstName + ", I am " + myAge + " years old, my size is " + myHeight);
+
+// challenge 1
+
+let num1 = 10;
+let num2 = 2;
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let quotient = num1 / num2;
+let product = num1 * num2;
+
+console.log("The sum is: " + sum);
+console.log("The difference is: " + difference);
+console.log("The quotient is: " + quotient);
+console.log("The product is: " + product);
+
+
+
+let radius = prompt("What is the radius?");
+var area = Math.PI * radius * radius;
+
+console.log("The area of a circle with a radius of " + radius + "cm is " + Math.round(area) + "cm^2.");
+
+// challenge 2
+
+var integerNumber;
+
+integerNumber = 1625;
+console.log("The value of integerNumber is: " + integerNumber);
+integerNumber = 375;
+
+alert(integerNumber + 125);
+
+// challenge 3
+
+var userAge = prompt("Please enter your age: ");
+alert("Thank you for your answer");
+alert("You are " + userAge + " years old");
+console.log(userAge);
+
+let aVariable;
+
+aVariable = 20;
+
+let otherVariable = 10;
+
+console.log(aVariable,otherVariable);
+
+aVariable=50;
+otherVariable=40;
+
+console.log(aVariable,otherVariable);
+
+const weekDays=7;
+console.log(weekDays);
+
